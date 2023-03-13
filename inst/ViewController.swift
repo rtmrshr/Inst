41217c86f573a31acceeebf6330d4fe68559be3a
rtmrshr <ratmir.sher@gmail.com>
2023-03-13 16:53:34 +0200

    Initial Commit

---
+++ inst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  inst
+//
+//  Created by  ratmir on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
